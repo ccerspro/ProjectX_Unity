e23216749bd8618a76d64ed22ac60787e034f278
Weilun <80656141+ccerspro@users.noreply.github.com>
2025-04-09 07:12:19 -0400

    save

--- Assets/Scripts/InfiniteCorridor.cs
+++ Assets/Scripts/InfiniteCorridor.cs
@@ -11,8 +11,8 @@ namespace ZhouSoftware{
     public class InfiniteCorridor : MonoBehaviour
     {
         List<GameObject> corridorList = new List<GameObject>();
-        private int corridorLength = 60;
-        private int corridorWidth = 32;
+        [SerializeField] private float corridorLength = 60;
+        [SerializeField] private float corridorWidth = 32;
         [SerializeField] GameObject normalPrefab;
         [SerializeField] List<GameObject> anomalyPrefab;
         [SerializeField] GameObject boundaryPrefab;
@@ -43,12 +43,12 @@ namespace ZhouSoftware{
             Vector3 current = corridorList[1].transform.position;
             Front = Instantiate(boundaryPrefab, new Vector3(current.x - 14 * direction, current.y, current.z + (corridorLength / 2 + 1) * direction), Quaternion.Euler(90, 0, 0));
             Rear = Instantiate(boundaryPrefab, new Vector3(current.x + 14 * direction, current.y, current.z - (corridorLength / 2 + 1) * direction), Quaternion.Euler(90, 0, 0));
-            //classSign = Instantiate(numberSign[0], new Vector3(current.x + 3.87f, current.y + 3, current.z), Quaternion.Euler(90, -90, 0));
+            classSign = Instantiate(numberSign[0], new Vector3(current.x + 3.87f, current.y + 3, current.z), Quaternion.Euler(90, -90, 0));
             Front.tag = "Front";
             Rear.tag = "Rear";
             Front.transform.SetParent(corridorList[1].transform);
             Rear.transform.SetParent(corridorList[1].transform);
-            //classSign.transform.SetParent(corridorList[1].transform);
+            classSign.transform.SetParent(corridorList[1].transform);
             level = 0; //start at level 0
         }
 
